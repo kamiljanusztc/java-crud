8819cf867f45fb6639b9f8bcebf22e388f66b69f
Kamil Janusz <kamiljanusztc@gmail.com>
2022-07-27 18:57:06 +0200

    Add board filters

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/trello")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("boards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.stream()
+                .filter(b -> b.getId() != null)
+                .filter(b -> b.getId() != null)
+                .filter(b -> b.getName().contains("Kodilla"))
+                .forEach(b ->
+                        { System.out.println(b.getId() + " " + b.getName()); }
+                        );
+    }
+}
