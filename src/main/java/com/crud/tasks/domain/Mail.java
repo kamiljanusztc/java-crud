f829c65cf4c02b7bf17712fe7359397933259013
Kamil Janusz <kamiljanusztc@gmail.com>
2022-08-06 15:39:15 +0200

    Change method Create trello card

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,12 +1,10 @@
 package com.crud.tasks.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class Mail {
     private final String mailTo;
     private final String subject;
