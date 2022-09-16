e23d95ef405c4338d4d3cf80a3da6ea1e462b92a
Kamil Janusz <kamiljanusztc@gmail.com>
2022-09-16 17:26:31 +0200

    Start Task controller tests

---
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -0,0 +1,48 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringJUnitWebConfig
+@WebMvcTest(TaskController.class)
+class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    TaskMapper taskMapper;
+
+    @MockBean
+    DbService dbService;
+
+    @Test
+    void shouldGetEmptyListOfTasks() throws Exception {
+        //Given
+        when(dbService.getAllTasks()).thenReturn(List.of());
+
+        //When & Then
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                    .get("/v1/tasks")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.status().is(200)) // or isOk()
+                    .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(0)));
+    }
+
+}
